b2b239be9f4ad4e9739eed75907fcaa5d213ef1d
Natthaphong PT. <natthaphong.piti.tat@gmail.com>
2025-11-07 13:51:44 +0700

    ADD: ASSEMBLY INFO

--- UserControlTest/My Project/AssemblyInfo.vb
+++ UserControlTest/My Project/AssemblyInfo.vb
@@ -6,21 +6,11 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTitle("SCADA SYSTEM")>
 <Assembly: AssemblyDescription("Supervisory Control and Data Acquisition System for Feed Mill Automation")>
 <Assembly: AssemblyCompany("TAT ENERGY AND ENGINEERING CO., LTD.")>
-<Assembly: AssemblyProduct("SCADA MASTER SYSTEM")> ' <<< PRODUCTION NAME
+<Assembly: AssemblyProduct("SCADA MASTER")>         '<<< PRODUCTION NAME
 <Assembly: AssemblyTrademark("SMART BATCHING")>
 <Assembly: AssemblyCulture("")>
-
-' Copyright
 <Assembly: AssemblyCopyright("Copyright © TAT ENERGY AND ENGINEERING CO., LTD. 2025")>
-
-' การเปิดให้ COM ใช้งาน
 <Assembly: ComVisible(False)>
-
-' GUID สำหรับ COM typelib (เมื่อเปิดให้ COM ใช้งาน)
 <Assembly: Guid("d5aa71ed-0fc2-4a43-92de-02a15f10e7c2")>
-
-'Assembly Version
-' Format: Major.Minor.Build.Revision
-' * can be used to have .NET automatically generate build and revision values.
-<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("1.0.*")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
